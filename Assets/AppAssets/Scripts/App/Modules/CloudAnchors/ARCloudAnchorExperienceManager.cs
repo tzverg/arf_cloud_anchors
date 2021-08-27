674b6fc201b194a1632297f256a37ebf345a7d4f
tzverg <tzverg@gmail.com>
2021-08-27 16:33:32 +0300

    add packages, register project and add API key

---
+++ Assets/AppAssets/Scripts/App/Modules/CloudAnchors/ARCloudAnchorExperienceManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace arf_cloud_anchors
+{
+    public class ARCloudAnchorExperienceManager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
